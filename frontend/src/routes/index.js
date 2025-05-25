7ba4c7cce32dbebf9469e3b35b55299989d90d8e
Jomachado2002 <josiasnicolas02@gmail.com>
2025-05-25 16:25:20 -0300

    Revert "cambios en el frontend category producto"
    This reverts commit 0e1cb5c42d1dcaf93249a01bd725964c9bc1e1a4.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
                 element: <MobileCategoriesPage />
             },
             {
-                path: "categoria-producto",  // ✅ Agregado parámetro
+                path: "categoria-producto/:categoryName", // ✅ Agregado parámetro
                 element: <CategoryProduct />
             },
             {
@@ -76,7 +76,7 @@ const router = createBrowserRouter([
                 element: <SearchProduct />
             },
 
-         
+          
            
             // ✅ Panel de admin separado
             {
